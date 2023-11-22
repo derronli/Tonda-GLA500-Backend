b1f517b556c4620d6ceff309e617fc9f50e49fcd
Derron Li <104483680+derronli@users.noreply.github.com>
2023-11-22 01:41:51 -0500

    write integration tests for Product POST endpoint

--- product-service/src/test/java/com/tonda/ecommerce/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/tonda/ecommerce/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,66 @@
 package com.tonda.ecommerce.productservice;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tonda.ecommerce.productservice.dto.ProductRequest;
+import com.tonda.ecommerce.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	// Tells test to use the MongoDB container -> straight from Testcontaners docs
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper; // Jackson module for converting POJO to JSON
+	@Autowired
+	private ProductRepository productRepository;
+
+	// we are using a dockerized temporary MongoDB Database for testing -> so we need to set the DB url
+	// in our application.properties to locate that database (currently we have hardcoded as our own DB right)
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString)
+		).andExpect(status().isCreated());
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Tonda Model S")
+				.description("Red Sedan 2023 model")
+				.price(BigDecimal.valueOf(1500))
+				.build();
+	}
 }
