de01e9cc2dc9dded4c11828be4a94b3fc9ba3ce3
Derron Li <104483680+derronli@users.noreply.github.com>
2023-12-02 13:19:43 -0500

    init notification service

---
+++ notification-service/src/test/java/com/tonda/ecommerce/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonda.ecommerce.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
