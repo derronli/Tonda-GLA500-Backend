a1587da3c08f2cbf09374e6ae1ddaf57990ba82d
Derron Li <104483680+derronli@users.noreply.github.com>
2023-12-02 13:41:17 -0500

    Configure Kafka Listener

--- notification-service/src/main/java/com/tonda/ecommerce/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/tonda/ecommerce/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,22 @@
 package com.tonda.ecommerce.notificationservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
+	// topic name defined in order service
+	@KafkaListener(topics = "notificationTopic")
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+		// send out email notification that order was placed (not implemented yet)
+		log.info("Received notification for order - {}", orderPlacedEvent.getOrderNumber());
+	}
 }
